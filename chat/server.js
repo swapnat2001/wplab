656a7d8199db7f9a07ab4337f5e1b534609c186c
swapnat2001 <89742451+swapnat2001@users.noreply.github.com>
2022-06-23 07:12:13 +0530

    Add files via upload

---
+++ chat/server.js
@@ -0,0 +1,20 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+const cors=require('cors');
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+})
